377d841c53c11ff16def1ea7092ca6b968c32432
wangjie <2280413000@qq.com>
2018-12-29 14:11:01 +0800

    breakpoint 1-2

--- resources/views/BreakPoint/breakpoint.blade.php
+++ resources/views/BreakPoint/breakpoint.blade.php
@@ -6,8 +6,25 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <script src="{{asset('assets/js/jquery-3.3.1.min.js')}}"></script>
 </head>
 <body>
     {{$name}}
+
+    <div id="btn_one">点击按钮</div>
+    <script>
+        $("#btn_one").on('click', function () {
+            $.ajax({
+                type: "GET",
+                url: "{{URL('breakpoint/uploadGet')}}",
+                success: function(data) {
+                    console.log(data);
+                },
+                beforeSend: function(xhr) {
+                    xhr.setRequestHeader("Range", "bytes=1-80");
+                }
+            });
+        })
+    </script>
 </body>
 </html>
